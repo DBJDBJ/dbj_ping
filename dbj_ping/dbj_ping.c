8a2dbf04d6ae3f148f2a44a9f491e846f8e6a790
dbjdbj <dbj@dbj.org>
2025-06-25 17:14:39 +0200

    complies and deploys

--- dbj_ping/dbj_ping.c
+++ dbj_ping/dbj_ping.c
@@ -6,6 +6,9 @@
 
 #pragma region Headers_and_Definitions
 
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
